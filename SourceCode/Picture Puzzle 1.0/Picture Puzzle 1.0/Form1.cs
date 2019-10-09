4fad2d5ecd004d8f441a1664e2efda07fb683728
AlvinDMartin <AlvinDMartin1999@users.noreply.github.com>
2019-10-09 12:14:40 +0700

    demo game 1
    demo game 1

---
+++ SourceCode/Picture Puzzle 1.0/Picture Puzzle 1.0/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Picture_Puzzle_1._0
+{
+    public partial class Form1 : Form
+    {
+        Point EmptyPoint;
+        ArrayList images = new ArrayList();
+        public Form1()
+        {
+            EmptyPoint.X = 180;
+            EmptyPoint.Y = 180;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnstartG_Click(object sender, EventArgs e)
+        {
+            foreach (Button b in pnGame.Controls)
+                b.Enabled = true;
+            Image orginal = Image.FromFile()
+        }
+    }
+}
